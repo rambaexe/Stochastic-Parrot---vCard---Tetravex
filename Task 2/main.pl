641c49c31be32bb21c1254c249b60ba0dd934cd1
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-04-02 01:37:47 +0100

    Task 2: Created AST
    AST created based on the given example.  Added RegEx for all properties.
    Bug: not working when adding random string to text file.
    Left to do:
    (potentially) do children in AST for parameters
    (potentially) run more than one vCard from example
    (mandatory) parse to HTML file
    (potentially) check cardinality of properties

--- Task 2/main.pl
+++ Task 2/main.pl
@@ -39,9 +39,56 @@ validate_required(InFile) :-
     delete(Content2, Line2, Content3),
 
     write("Required properties satisfied.\n"),
-    % print what is in AST
-    write(AST3).
 
+    % check FN property and append it to AST
+    nth0(0, Content3, Line3),
+    check_FN(Line3, FN_AST),
+    append_at_index(2, AST3, FN_AST, AST4),
+    delete(Content3, Line3, Content4),
+
+    % check if all required properties are satisfied
+    validate_properties(Content4, AST_Properties, AST_Properties_Out),
+
+    % append empty list to AST at index 3
+    append_at_index(3, AST4, AST_Properties_Out, AST5),
+
+    % print what is in AST
+    write(AST5).
+
+
+validate_properties([], AST_Properties, AST_Properties) :- 
+    write("Properties are valid."),nl.
+
+validate_properties(Content, AST_Properties_In, AST_Properties_Out) :-
+    Content = [Line| _],
+    % if condition -> all properties are checked here
+    (   check_N(Line, AST_property);
+        check_BDAY(Line, AST_property);
+        check_ANNIVERSARY(Line, AST_property);
+        check_GENDER(Line, AST_property);
+        check_LANG(Line, AST_property);
+        check_ORG(Line, AST_property); 
+        check_ADR(Line, AST_property);
+        check_TEL(Line, AST_property);
+        check_EMAIL(Line, AST_property);
+        check_GEO(Line, AST_property);
+        check_KEY(Line, AST_property);
+        check_TZ(Line, AST_property);
+        check_URL(Line, AST_property))
+     
+
+    % if property satisfied
+    -> 
+    append(AST_Properties_In, [AST_property], AST_Properties1),
+    delete(Content, Line, Content1),
+    validate_properties(Content1, AST_Properties1,AST_Properties_Out);
+
+    % property not satisfied
+    nth0(0, Content, Line),
+    write("Error: "), write(Line), write("\n"),
+    write("Not all properties satisfied.\n"),
+    validate_properties([],_,_),
+    false.
 
 % read file content and put it in a list
 read_file(InFile, Content) :-
@@ -58,3 +105,9 @@ read_lines(Stream, Content) :-
         )
     ;   Content = []
     ).
+
+% append at index 
+append_at_index(Index, List, Value, Result) :-
+    length(Prefix, Index),
+    append(Prefix, Suffix, List),
+    append(Prefix, [Value|Suffix], Result).
--- Task 2/vcardvalidation.pl
+++ Task 2/vcardvalidation.pl
@@ -1,7 +1,6 @@
-library(pcre).
-
 check_begin(Line, AST):-
     re_match("BEGIN:VCARD", Line) ->
+    write("BEGIN:VCARD found.\n"),
     split_string(Line, ":", "", [Key, Value]),
     AST = [Key, Value],
     true;
@@ -9,6 +8,7 @@ check_begin(Line, AST):-
 
 check_end(Line, AST):-
     re_match("END:VCARD", Line) -> 
+    write("END:VCARD found.\n"),
     split_string(Line, ":", "", [Key, Value]),
     AST = [Key, Value],
     true;
@@ -16,8 +16,154 @@ check_end(Line, AST):-
 
 check_version(Line, AST):-
     re_match("VERSION:4.0", Line) -> 
+    write("VERSION:4.0 found.\n"),
     split_string(Line, ":", "", [Key, Value]),
     AST = [Key, Value],
     true;
     write("Error: No VERSION:4.0. vCard not valid."), false.
 
+check_FN(Line, AST_property):-
+    re_match("FN:.+", Line) -> 
+    write("FN: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_N(Line, AST_property):-
+    re_match("N:[^;]*;[^;]*;[^;]*;[^;]*;[^;]*", Line) -> 
+    write("N: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_BDAY(Line, AST_property):-
+    (   re_match("BDAY:\\d{8}", Line);
+        re_match("BDAY:--\\d{4}", Line);
+        re_match("BDAY:\\d{4}-\\d{2}-\\d{2}", Line);
+        re_match("BDAY;\\d{8}T\\d{6}Z", Line);
+        re_match("BDAY;VALUE=text:.+", Line))
+    -> 
+    write("BDAY: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_ANNIVERSARY(Line, AST_property):-
+    (   re_match("ANNIVERSARY:\\d{8}T\\d{4}-\\d{4}", Line);
+        re_match("ANNIVERSARY:\\d{8}$", Line))
+    -> 
+    write("ANNIVERSARY: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_GENDER(Line, AST_property):-
+    (re_match("GENDER:[MFONU]+;?.*", Line);
+    re_match("GENDER:;\\S+", Line))
+    -> 
+    write("Gender: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_LANG(Line, AST_property):-
+    (re_match("LANG;TYPE=.+;PREF=\\d+:.+", Line);
+    re_match("LANG;TYPE=.+:.+", Line);
+    re_match("LANG;PREF=\\d+:.+", Line)) 
+    -> 
+    write("LANG: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_ORG(Line, AST_property):-
+    (re_match("ORG:.+", Line);
+    re_match("ORG;TYPE=.+:.+", Line))
+    -> 
+    write("ORG: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_ADR(Line, AST_property):-
+    (re_match("ADR;TYPE=.+:.*;.*;.*;.*;.*;.*", Line))
+    -> 
+    write("ADR: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_TEL(Line, AST_property):-
+    (re_match("TEL;VALUE=.+;TYPE=.+:tel:\\+?\\d+(-\\d+)*(;ext=\\d+)?$", Line);
+    re_match("TEL;VALUE=.+;PREF=\\d+;TYPE=.+:tel:\\+?\\d+(-\\d+)*(;ext=\\d+)?$", Line);
+    re_match("TEL;VALUE=.+;TYPE=.+;PREF=\\d+:tel:\\+?\\d+(-\\d+)*(;ext=\\d+)?$", Line))
+    -> 
+    write("TEL: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value, Value2]),
+    AST_property = [Key, Value:Value2],
+    true;
+    false.
+
+check_EMAIL(Line, AST_property):-
+    (re_match("EMAIL;TYPE=.+:.+@.+\\..+", Line);
+    re_match("EMAIL;PREF=\\d+:.+@.+\\..+", Line))
+    -> 
+    write("EMAIL: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_GEO(Line, AST_property):-
+    (re_match("GEO:geo:-?\\d+(\\.\\d+)?,-?\\d+(\\.\\d+)?", Line))
+    ->
+    write("GEO: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    re_match("GEO;TYPE=.+:geo:-?\\d+(\\.\\d+)?,-?\\d+(\\.\\d+)?", Line)
+    -> 
+    write("GEO: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value, Value2]),
+    AST_property = [Key, Value:Value2],
+    true;
+    false.
+
+check_KEY(Line, AST_property):-
+    (re_match("KEY:.+", Line);
+    re_match("KEY;MEDIATYPE=.+:.+", Line);
+    re_match("KEY;TYPE=.+;VALUE=.+:.+", Line))
+    -> 
+    write("KEY: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value, Value2]),
+    AST_property = [Key, Value:Value2],
+    true;
+    false.
+
+check_TZ(Line, AST_property):-
+    (re_match("TZ:-?\\d{4}", Line);
+    re_match("TZ:.+/.*", Line))
+    -> 
+    write("TZ: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value]),
+    AST_property = [Key, Value],
+    true;
+    false.
+
+check_URL(Line, AST_property):-
+    (re_match("URL:http.+", Line);
+    re_match("URL;TYPE=.+:http.+", Line))
+    -> 
+    write("URL: found and valid.\n"),
+    split_string(Line, ":", "", [Key, Value, Value2]),
+    AST_property = [Key, Value:Value2],
+    true;
+    false.
