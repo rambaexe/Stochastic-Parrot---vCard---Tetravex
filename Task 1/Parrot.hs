9432922d96d41adcbc3fd30dbdb28738c8012cb5
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-04-04 17:00:19 +0100

    test on big corpus

--- Task 1/Parrot.hs
+++ Task 1/Parrot.hs
@@ -56,20 +56,21 @@ trigramdict3 cont = foldr updateModel Map.empty (triples cont)
 
 getContent = do  
     let book1 = "Text Files/A_Book_of_Discovery.txt"
-        book2 = "Text Files/A_Literary_and_Historical_Atlas_of_Asia.txt"
-        book3 = "Text Files/Celebrated_Travels.txt"
+        -- book2 = "Text Files/A_Literary_and_Historical_Atlas_of_Asia.txt"
+        -- book3 = "Text Files/Celebrated_Travels.txt"
         book4 = "Text Files/Commercial_Geography.txt"
         book5 = "Text Files/Darwin_and_Modern_Science_by_A_C_Seward.txt"
         book6 = "Text Files/Days_of_the_Discoverers.txt"
-        book7 = "Text Files/Outlines_of_Dairy_Bacteriology.txt"
+        -- book7 = "Text Files/Outlines_of_Dairy_Bacteriology.txt"
         book8 = "Text Files/Seaside_Studies_in_Natural_History.txt"
         book9 = "Text Files/The_Beauties_of_Nature.txt"
         book10 = "Text Files/The_Earth_and_its_inhabitants_Volume1_Europe.txt"
         
         -- filePaths = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10]
+        filePaths = [book1, book4, book5, book6, book8, book9, book10]
 
         test = "Text Files/test.txt"
-        filePaths = [test]
+        --filePaths = [test]
 
         -- filePaths = [book1]
         
@@ -127,8 +128,8 @@ main = do
 
     --print "Unigram:"
     --print unigram
-    putStrLn "Bigram:" 
-    print bigram
+    --putStrLn "Bigram:" 
+    --print bigram
     --print "Trigram:"
     --print trigram
 
