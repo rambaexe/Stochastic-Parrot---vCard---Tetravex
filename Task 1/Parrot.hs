5b3b07441dffa3f9214d2e78330395987512ce21
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-04-04 13:00:08 +0100

    Update Parrot.hs
    Created new file to work on the language models. Created preprocessing on the content. Created the language models dictionaries for unigram, bigram and trigram. Saving the words and the words following without the count.

--- Task 1/Parrot.hs
+++ Task 1/Parrot.hs
@@ -5,6 +5,8 @@ import Data.Map.Strict (Map)
 import qualified Data.Map as Map
 import Data.Typeable (typeOf)
 import System.Random
+import Data.Time.Clock
+import Data.Time.LocalTime
 
 readFileToList :: [FilePath] -> IO [String]
 readFileToList filePaths = fmap concat (mapM readFileToWords filePaths)
@@ -15,12 +17,7 @@ readFileToWords filePath = do
     --print (take 100 (words contents))
     return (map (map toLower) (words contents))
 
--- unigram dictionary: save all the words without accounting for the next word
-unigramdict cont = foldr updateModel Map.empty cont
-  where
-    updateModel word unigramMap = case Map.lookup word unigramMap of
-        Just count -> Map.insert word (count + 1) unigramMap
-        Nothing -> Map.insert word 1 unigramMap
+
 
 -- bigram dictionary: save all the words with the next word and the count of the next word
 bigramdict cont = foldr updateModel Map.empty (pairs cont)
@@ -40,7 +37,25 @@ bigramdict cont = foldr updateModel Map.empty (pairs cont)
     updateWords word ((w, count) : ws)
         | w == word = (w, count + 1) : ws
         | otherwise = (w, count) : updateWords word ws
-    -- updateWords _ [] = []
+
+-- bigram dictionary: save all the words with the next word following that; do not save the count of the next word
+bigramdict2 cont = foldr updateModel Map.empty (pairs cont)
+  where
+    pairs :: [a] -> [(a, a)]
+    pairs [] = []
+    pairs [_] = []
+    pairs (x:y:conttail) = (x, y) : pairs (y:conttail)
+
+    updateModel :: (String, String) -> Map String [String] -> Map String [String]
+    updateModel (x, y) bigramMap = case Map.lookup x bigramMap of
+        Just ys -> Map.insert x (updateWords y ys) bigramMap
+        Nothing -> Map.insert x [y] bigramMap
+
+    updateWords :: String -> [String] -> [String]
+    updateWords word [] = [word]
+    updateWords word (w : ws)
+        | w == word = w : ws
+        | otherwise = w : updateWords word ws
 
 -- trigram dictionary: save all the words with the next two words and the count of the next two words
 trigramdict cont = foldr updateModel Map.empty (triples cont)
@@ -51,17 +66,51 @@ trigramdict cont = foldr updateModel Map.empty (triples cont)
     triples [_, _] = []
     triples (x:y:z:conttail) = (x, y, z) : triples (y:z:conttail)
 
-    updateModel :: (String, String, String) -> Map String [(String, String, Int)] -> Map String [(String, String, Int)]
     updateModel (x, y, z) trigramMap = case Map.lookup x trigramMap of
         Just ys -> Map.insert x (updateWords y z ys) trigramMap
         Nothing -> Map.insert x [(y, z, 1)] trigramMap
 
-    updateWords :: String -> String -> [(String, String, Int)] -> [(String, String, Int)]
     updateWords word1 word2 [] = [(word1, word2, 1)]
     updateWords word1 word2 ((w1, w2, count) : ws)
         | w1 == word1 && w2 == word2 = (w1, w2, count + 1) : ws
         | otherwise = (w1, w2, count) : updateWords word1 word2 ws
-    -- updateWords _ [] = []
+
+-- trigram dictionary 2: save all the words with the next two words following that; do not save the count of the next two words
+trigramdict2 cont = foldr updateModel Map.empty (triples cont)
+  where
+    triples :: [a] -> [(a, a, a)]
+    triples [] = []
+    triples [_] = []
+    triples [_, _] = []
+    triples (x:y:z:conttail) = (x, y, z) : triples (y:z:conttail)
+
+    updateModel (x, y, z) trigramMap = case Map.lookup x trigramMap of
+        Just ys -> Map.insert x (updateWords y z ys) trigramMap
+        Nothing -> Map.insert x [(y, z)] trigramMap
+
+    updateWords word1 word2 [] = [(word1, word2)]
+    updateWords word1 word2 ((w1, w2) : ws)
+        | w1 == word1 && w2 == word2 = (w1, w2) : ws
+        | otherwise = (w1, w2) : updateWords word1 word2 ws
+
+-- trigram dictionary: save each two words as key and the next word as value
+trigramdict3 cont = foldr updateModel Map.empty (triples cont)
+  where
+    triples :: [a] -> [(a, a, a)]
+    triples [] = []
+    triples [_] = []
+    triples [_, _] = []
+    triples (x:y:z:conttail) = (x, y, z) : triples (y:z:conttail)
+
+    updateModel (x, y, z) trigramMap = case Map.lookup (x, y) trigramMap of
+        Just ys -> Map.insert (x, y) (updateWords z ys) trigramMap
+        Nothing -> Map.insert (x, y) [z] trigramMap
+
+    updateWords word [] = [word]
+    updateWords word (w : ws)
+        | w == word = w : ws
+        | otherwise = w : updateWords word ws
+
 
 getContent = do  
     let book1 = "Text Files/A_Book_of_Discovery.txt"
@@ -78,8 +127,9 @@ getContent = do
         -- filePaths = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10]
 
         test = "Text Files/test.txt"
-        
         filePaths = [test]
+
+        -- filePaths = [book1]
         
     bookContent <- readFileToList filePaths
     return bookContent
@@ -88,23 +138,22 @@ getContent = do
     -- mapM_ putStrLn (take 100 bookContent)
 
 
-preprocess :: [String] -> [String]
 preprocess [] = []
 preprocess (x:xs) = (map toLower (filter (\c -> isAlphaNum c || isSpace c) x)) : preprocess xs
 
+-- 
 
 main = do
     bookContent <- getContent
     let processesedContent = preprocess bookContent
-    print (take 100 processesedContent)
 
-    let unigram = unigramdict processesedContent
-    let bigram = bigramdict processesedContent
-    let trigram = trigramdict processesedContent
-    -- print heading "Unigram:", "Bigram:", "Trigram:
+    let unigram = processesedContent
+    let bigram = bigramdict2 processesedContent
+    let trigram = trigramdict3 processesedContent
+
     print "Unigram:"
-    printNgram unigram
-    print "Bigram:"
-    printNgram bigram
+    print unigram
+    print "Bigram:" 
+    print bigram
     print "Trigram:"
-    printNgram trigram
+    print trigram
