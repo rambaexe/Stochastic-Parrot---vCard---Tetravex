c5648d738d040b1d83392ed2ab12be1768576e0e
Teodor-Andrei Ramba <rambat@coventry.ac.uk>
2024-04-05 16:02:55 +0100

    prepared parrot for video
    deleted not needed files; deleted the initial attempt Stochastic_Parrot.hs . Edited Parrot.hs for video.

--- Task 1/Parrot.hs
+++ Task 1/Parrot.hs
@@ -16,6 +16,34 @@ readFileToWords filePath = do
     contents <- readFile filePath
     return (map (map toLower) (words contents))
 
+getContent = do  
+    let book1 = "Text Files/A_Book_of_Discovery.txt"
+        -- book2 = "Text Files/A_Literary_and_Historical_Atlas_of_Asia.txt"
+        book3 = "Text Files/Celebrated_Travels.txt"
+        book4 = "Text Files/Commercial_Geography.txt"
+        book5 = "Text Files/Darwin_and_Modern_Science_by_A_C_Seward.txt"
+        book6 = "Text Files/Days_of_the_Discoverers.txt"
+        -- book7 = "Text Files/Outlines_of_Dairy_Bacteriology.txt"
+        book8 = "Text Files/Seaside_Studies_in_Natural_History.txt"
+        book9 = "Text Files/The_Beauties_of_Nature.txt"
+        book10 = "Text Files/The_Earth_and_its_inhabitants_Volume1_Europe.txt"
+        
+        
+        filePaths = [book1, book3, book4, book5, book6, book8, book9, book10]
+
+        -- test = "Text Files/test.txt"
+        -- filePaths = [test]
+        
+    bookContent <- readFileToList filePaths
+    return bookContent
+
+-- unigram dictionary with count of each word
+-- unigramdict cont = foldr updateModel Map.empty cont
+--     where
+--         updateModel word unigramMap = case Map.lookup word unigramMap of
+--             Just count -> Map.insert word (count + 1) unigramMap
+--             Nothing -> Map.insert word 1 unigramMap
+
 -- bigram dictionary: save all the words with the next word following that; do not save the count of the next word
 bigramdict2 cont = foldr updateModel Map.empty (pairs cont)
   where
@@ -35,46 +63,44 @@ bigramdict2 cont = foldr updateModel Map.empty (pairs cont)
         | w == word = w : ws
         | otherwise = w : updateWords word ws
 
+-- bigram dictionary old: save dictionary with count of the next word
+-- bigramdict cont = foldr updateModel Map.empty (pairs cont)
+--     where
+--         pairs :: [a] -> [(a, a)]
+--         pairs [] = []
+--         pairs [_] = []
+--         pairs (x:y:conttail) = (x, y) : pairs (y:conttail)
+    
+--         updateModel :: (String, String) -> Map String [(String, Int)] -> Map String [(String, Int)]
+--         updateModel (x, y) bigramMap = case Map.lookup x bigramMap of
+--             Just ys -> Map.insert x (updateWords y ys) bigramMap
+--             Nothing -> Map.insert x [(y, 1)] bigramMap
+    
+--         updateWords :: String -> [(String, Int)] -> [(String, Int)]
+--         updateWords word [] = [(word, 1)]
+--         updateWords word ((w, count) : ws)
+--             | w == word = (w, count + 1) : ws
+--             | otherwise = (w, count) : updateWords word ws
+
 -- trigram dictionary: save each two words as key and the next word as value
-trigramdict3 cont = foldr updateModel Map.empty (triples cont)
-  where
-    triples :: [a] -> [(a, a, a)]
-    triples [] = []
-    triples [_] = []
-    triples [_, _] = []
-    triples (x:y:z:conttail) = (x, y, z) : triples (y:z:conttail)
+-- trigramdict3 cont = foldr updateModel Map.empty (triples cont)
+--   where
+--     triples :: [a] -> [(a, a, a)]
+--     triples [] = []
+--     triples [_] = []
+--     triples [_, _] = []
+--     triples (x:y:z:conttail) = (x, y, z) : triples (y:z:conttail)
 
-    updateModel (x, y, z) trigramMap = case Map.lookup (x, y) trigramMap of
-        Just ys -> Map.insert (x, y) (updateWords z ys) trigramMap
-        Nothing -> Map.insert (x, y) [z] trigramMap
+--     updateModel (x, y, z) trigramMap = case Map.lookup (x, y) trigramMap of
+--         Just ys -> Map.insert (x, y) (updateWords z ys) trigramMap
+--         Nothing -> Map.insert (x, y) [z] trigramMap
 
-    updateWords word [] = [word]
-    updateWords word (w : ws)
-        | w == word = w : ws
-        | otherwise = w : updateWords word ws
+--     updateWords word [] = [word]
+--     updateWords word (w : ws)
+--         | w == word = w : ws
+--         | otherwise = w : updateWords word ws
 
-getContent = do  
-    let book1 = "Text Files/A_Book_of_Discovery.txt"
-        -- book2 = "Text Files/A_Literary_and_Historical_Atlas_of_Asia.txt"
-        book3 = "Text Files/Celebrated_Travels.txt"
-        book4 = "Text Files/Commercial_Geography.txt"
-        book5 = "Text Files/Darwin_and_Modern_Science_by_A_C_Seward.txt"
-        book6 = "Text Files/Days_of_the_Discoverers.txt"
-        -- book7 = "Text Files/Outlines_of_Dairy_Bacteriology.txt"
-        book8 = "Text Files/Seaside_Studies_in_Natural_History.txt"
-        book9 = "Text Files/The_Beauties_of_Nature.txt"
-        book10 = "Text Files/The_Earth_and_its_inhabitants_Volume1_Europe.txt"
-        
-        -- filePaths = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10]
-        filePaths = [book1, book3, book4, book5, book6, book8, book9, book10]
 
-        test = "Text Files/test.txt"
-        --filePaths = [test]
-
-        -- filePaths = [book1]
-        
-    bookContent <- readFileToList filePaths
-    return bookContent
 
 
 preprocess [] = []
@@ -116,14 +142,19 @@ getWordFromBigramList word i bigram = case Map.lookup word bigram of
     Just ys -> ys !! i
     Nothing -> ""
 
--- main function: get content from files, preprocess the content, create unigram, bigram and trigram dictionaries, generate random sentences
+
+
+-- main function: get content from files, preprocess the content, create unigram, bigram dictionaries, generate random sentences
 main = do
     bookContent <- getContent
-    let processesedContent = preprocess bookContent
+    let prepContent = preprocess bookContent
+    -- print number of words in prepContent
+    putStrLn "Number of words: "
+    print (length prepContent)
 
-    let unigram = processesedContent
-    let bigram = bigramdict2 processesedContent
-    let trigram = trigramdict3 processesedContent
+    let unigram = prepContent
+    let bigram = bigramdict2 prepContent
+    -- let trigram = trigramdict3 prepContent
 
     --print "Unigram:"
     --print unigram
@@ -136,6 +167,6 @@ main = do
     createRandomSentenceUnigram unigram 20
     putStrLn "Bigram sentence: "
     createRandomSentenceBigram bigram 20
-    -- print(getWordFromBigramList "Alabama" 0 bigram)
+    
 
     
